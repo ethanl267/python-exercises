c39731c6104ad9a1dd1224c80f3583ee8016d4e4
ethanl267 <71071509+ethanl267@users.noreply.github.com>
2020-09-21 11:10:31 +0200

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+class_name = "class 2"
+
+print("I was coded in " + class_name)
+
+class_students = "19"
+
+print("there are " + class_students + " students in " + class_name) 
